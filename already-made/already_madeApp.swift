ff2523401fa7703424dfe0d401f4397aefd29ef3
Igor Jakus <igorjakus@protonmail.com>
2023-09-16 14:01:52 +0200

    Initial Commit

---
+++ already-made/already_madeApp.swift
@@ -0,0 +1,17 @@
+//
+//  already_madeApp.swift
+//  already-made
+//
+//  Created by Igor  Jakus on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct already_madeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
